33bae3ec46d5a998d4ef0f0d581e82ca65fa4924
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-06-30 17:29:10 -0400

    Add safeguard in autoload.php

--- autoload.php
+++ autoload.php
@@ -1,4 +1,7 @@
 <?php declare(strict_types=1); namespace Phprelude;
 require_once __DIR__ . '/src/core.php';
 
-\Phprelude\Core\require_directory(__DIR__ . '/src');
+/* Proceed only if there are no function collisions TODO: refine */
+if (!function_exists('identity')) {
+    \Phprelude\Core\require_directory(__DIR__ . '/src');
+}
