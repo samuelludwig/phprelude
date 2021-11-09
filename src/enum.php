c42362b3c1e9ee3e637c7a974d547e38efde06b6
Samuel Ludwig <SLudwig.dev@gmail.com>
2021-11-09 15:46:03 -0500

    Fix namespace of zip_as_field function calls

--- src/enum.php
+++ src/enum.php
@@ -930,22 +930,6 @@ function lgroup_by($key): Closure {
     return fn($a) => group_by($a, $key);
 }
 
-function zip_as_field(
-    array $host,
-    array $data,
-    string $field_name
-): array {
-    $length = min(count($host), count($data));
-    return p\p(
-        $host, 
-        Enum\ltake($length), 
-        Enum\lmap(fn($x) => is_array($x) ? $x : [$x]),
-        Enum\lmap_with_index(
-            fn($index, $a) => Enum\set_key_val($a, $field_name, $data[$index])
-        )
-    );
-}
-
 /** 
  * Zips together two arrays, the $host, and $data, where zipped $data elements
  * are appended to the corresponding $host element as under the $field_name key.
@@ -955,6 +939,22 @@ function zip_as_field(
  *
  * Turns elements into an array if they aren't already.
  */
+function zip_as_field(
+    array $host,
+    array $data,
+    string $field_name
+): array {
+    $length = min(count($host), count($data));
+    return p\p(
+        $host, 
+        ltake($length), 
+        lmap(fn($x) => is_array($x) ? $x : [$x]),
+        lmap_with_index(
+            fn($index, $a) => set_key_val($a, $field_name, $data[$index])
+        )
+    );
+}
+
 function lzip_as_field(array $data, string $field_name): Closure {
   return fn($a) => zip_as_field($a, $data, $field_name);
 }
